2be758a7f80939c47867d5f7daf964639b83b58d
Colin Rice <dah4k0r@gmail.com>
2012-01-08 15:54:25 -0800

    Make it not specify a sleep time

--- gevent_dht/set_store.py
+++ gevent_dht/set_store.py
@@ -94,7 +94,7 @@ class TestStore(unittest.TestCase):
         net2.set_handler.add('lp_blah', 'bar')
         with gevent.Timeout(3):
             while len(net.set_handler.dict) + len(net2.set_handler.dict) < 2:
-                gevent.sleep(0.001)
+                gevent.sleep()
         self.assertEqual(net.set_handler.get('hi'), 'bar')
         self.assertEqual(net.set_handler.get('lp_blah'), ['hi','bar'])
 

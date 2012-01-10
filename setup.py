ddf3f70cc53213d5b667c37dff393a1948bc0fc5
Colin Rice <dah4k0r@gmail.com>
2012-01-09 17:45:55 -0800

    Updated with proper license

--- setup.py
+++ setup.py
@@ -8,4 +8,11 @@ setup(name='gevent_dht',
         author_email='dah4k0r@gmail.com',
         packages = ['gevent_dht'],
         url='github.com/c00w/gevent_dht',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: System :: Networking',
+            'License :: OSI Approved :: MIT License',
+            ]
+        license = 'MIT Expat License'
         )
